2ff6fdea7c7ab3f200cad19d3de5d1db78fd2985
Zendrix Seiryu <zendrix.seiryu.dev@gmail.com>
2024-07-12 19:31:39 -0500

    menuvar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavmenuComponent } from "./components/navmenu/navmenu.component";
+import { RedesComponent } from "./components/redes/redes.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavmenuComponent, RedesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NavmenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+import { RedesComponent } from './components/redes/redes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+];
